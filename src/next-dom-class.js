a9220f27431a79553d40b9db6a4ec6ae964b2a56
afeiship <1290657123@qq.com>
2019-02-28 09:48:32 +0800

    add: remove/replace/toggle

--- src/next-dom-class.js
+++ src/next-dom-class.js
@@ -2,6 +2,7 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var classCache = {};
+  var BLANK_RE = /\s+/g;
 
   // @thanks to zeptojs:
   function classRE(inName) {
@@ -13,10 +14,37 @@
   // https://github.com/madrobby/zepto/blob/master/src/zepto.js
   var NxDomClass = nx.declare('nx.DomClass', {
     statics: {
-      add: function() {},
-      remove: function() {},
-      replace: function() {},
-      toggle: function() {},
+      add: function(inElement, inClassName) {
+        var classList = [];
+        var oldClass = inElement.className;
+        var space = oldClass ? ' ' : '';
+        inClassName.split(BLANK_RE).forEach(function(cls) {
+          !this.contains(inElement, cls) && classList.push(cls);
+        }, this);
+
+        if (classList.length) {
+          inElement.className = oldClass + space + classList.join(' ');
+        }
+      },
+      remove: function(inElement, inClassName) {
+        var oldClass = inElement.className;
+        var currentClass = oldClass;
+        inClassName.split(BLANK_RE).forEach(function(cls) {
+          if (this.contains(inElement, cls)) {
+            currentClass = currentClass.replace(inClassName, '');
+          }
+        }, this);
+        inElement.className = currentClass.trim();
+      },
+      replace: function(inElement, inClassName1, inClassName2) {
+        var oldClass = inElement.className;
+        var currentClass = oldClass.replace(inClassName1, inClassName2);
+        inElement.className = currentClass.trim();
+      },
+      toggle: function(inElement, inClassName) {
+        var action = this.contains(inElement, inClassName) ? 'remove' : 'add';
+        this[action](inElement, inClassName);
+      },
       contains: function(inElement, inClassName) {
         if (!inClassName) return false;
         var className = inElement.className;
